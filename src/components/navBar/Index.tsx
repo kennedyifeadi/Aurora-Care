ec184c084e21d4188d839b269ee3dda8f160060f
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-08-18 17:33:37 +0100

    feat: update TopNav component to display user full name and initials dynamically

--- src/components/navBar/Index.tsx
+++ src/components/navBar/Index.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import { SearchBar } from "./SearchBar"
 import { RiNotification2Line } from "react-icons/ri";
 
 export const TopNav = () => {
+  const user = localStorage.getItem("userData");
+  const userData = user ? JSON.parse(user) : null;
+  const initials = userData ? userData[0].trim().split(" ") : ['U', 'U'];
+  const fullName = userData ? userData[0] : 'User';
+  useEffect(() => {
+   console.log("User Data:", userData);
+  }, [userData]);
   return (
     <div className="w-full h-16 border-b border-gray-200 flex items-center justify-between px-4">
       <SearchBar />
@@ -10,8 +18,10 @@ export const TopNav = () => {
           <RiNotification2Line className="text-gray-500" size={20} />
         </div>
         <div className="flex w-max h-full gap-2 justify-center items-center">
-          <h1 className="font-medium text-[18px]">Ifeadi Kennedy</h1>
-          <span className="rounded-full h-10 w-10 bg-[#d87cdc] text-white font-medium text-[18px] flex justify-center items-center">IK</span>
+          <h1 className="font-medium text-[18px] capitalize">{fullName}</h1>
+          <span className="rounded-full h-10 w-10 bg-[#d87cdc] text-white uppercase font-medium text-[18px] flex justify-center items-center">
+            {(initials[0]?.[0]) + (initials[1]?.[0] || '')}
+          </span>
         </div>
       </div>
     </div>
