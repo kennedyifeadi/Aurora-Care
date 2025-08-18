27723f141eea0f3b20d23a85850460d3e6d900e6
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-08-18 17:33:52 +0100

    refactor: clean up MainLayout component structure and formatting

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,17 +1,17 @@
-import { Outlet } from "react-router-dom"
-import { SideBar } from "../components/sideBar/Index"
-import { TopNav } from "../components/navBar/Index"
+import { SideBar } from "../components/sideBar/Index";
+import { TopNav } from "../components/navBar/Index";
+import { MainRoute } from "../routes/MainRoute";
 
 export const MainLayout = () => {
   return (
     <div className="flex max-h-screen w-full h-screen ">
-    <div className="w-max h-full">
+      <div className="w-max h-full">
         <SideBar />
-    </div>
-    <div className="flex-1 h-full flex flex-col">
+      </div>
+      <div className="flex-1 h-full flex flex-col">
         <TopNav />
-        <Outlet />
+        <MainRoute />
+      </div>
     </div>
-    </div>
-  )
+  );
 }
