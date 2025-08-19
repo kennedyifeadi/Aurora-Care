38dfdf557c04baef92d296e6efe1999aaf4b9095
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-08-19 11:49:59 +0100

    fix: correct logout display logic in Sidebar component

--- src/components/sideBar/Index.tsx
+++ src/components/sideBar/Index.tsx
@@ -190,7 +190,7 @@ export const SideBar = () => {
           <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
           <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8z"></path>
         </svg> : <FiLogOut size={20} />}
-        {(!collapsed || isMobile) && (<span className="flex"> Logout</span>)}
+        {(!collapsed && (<span className="flex"> Logout</span>))}
       </div>
     </>
   );
