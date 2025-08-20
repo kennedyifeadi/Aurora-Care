0dee75bbade084517d067a641934a3e89c73f987
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-08-20 18:29:09 +0100

    feat: implement calendar layout with responsive design

--- src/components/dashboardComp/Calender.tsx
+++ src/components/dashboardComp/Calender.tsx
@@ -1,6 +1,9 @@
 
 export const Calender = () => {
-  return (
-    <div>Calender</div>
-  )
+    return (
+        <div className="h-max w-full flex gap-8 flex-wrap">
+            <div className="w-[360px] md:w-[500px] h-[300px] md:h-[400px] rounded-xl bg-white hover:shadow-md border border-gray-200 "></div>
+            <div className="w-[360px] md:w-[500px] h-[300px] md:h-[400px] rounded-xl bg-white hover:shadow-md border border-gray-200 "></div>
+        </div>
+    )
 }
