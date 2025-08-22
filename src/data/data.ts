85be530cf2a64d8f7ddb9b8660eb83999399e459
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-08-22 20:35:56 +0100

    feat: enhance TopNav with current date display and add ChildPastData and MotherPastData

---
+++ src/data/data.ts
@@ -0,0 +1,104 @@
+export const ChildPastData = [
+    {
+        name: 'Monday',
+        heartBeat: 130,
+        time: '10:00 AM',
+        date: '2023-10-02'
+    },
+    {
+        name: 'Tuesday',
+        heartBeat: 135,
+        time: '10:00 AM',
+        date: '2023-10-03'
+    },
+    {
+        name: 'Wednesday',
+        heartBeat: 108,
+        time: '10:00 AM',
+        date: '2023-10-04'
+    },
+    {
+        name: 'Thursday',
+        heartBeat: 70,
+        time: '10:00 AM',
+        date: '2023-10-05'
+    },
+    {
+        name: 'Friday',
+        heartBeat: 132,
+        time: '10:00 AM',
+        date: '2023-10-06'
+    },
+    {
+        name: 'Saturday',
+        heartBeat: 129,
+        time: '10:00 AM',
+        date: '2023-10-07'
+    },
+    {
+        name: 'Sunday',
+        heartBeat: 140,
+        time: '10:00 AM',
+        date: '2023-10-08'
+    }
+]
+
+
+export const MotherPastData = [
+    {
+        name: 'Monday',
+        heartBeat: 80,
+        cervicalDialation: 1,
+        contractions: 5,
+        time: '10:00 AM',
+        date: '2023-10-02'
+    },
+    {
+        name: 'Tuesday',
+        heartBeat: 75,
+        cervicalDialation: 1,
+        contractions: 4,
+        time: '10:00 AM',
+        date: '2023-10-03'
+    },
+    {
+        name: 'Wednesday',
+        heartBeat: 78,
+        cervicalDialation: 3,
+        contractions: 4,
+        time: '10:00 AM',
+        date: '2023-10-04'
+    },
+    {
+        name: 'Thursday',
+        heartBeat: 70,
+        cervicalDialation: 2,
+        contractions: 3,
+        time: '10:00 AM',
+        date: '2023-10-05'
+    },
+    {
+        name: 'Friday',
+        heartBeat: 72,
+        cervicalDialation: 2,
+        contractions: 4,
+        time: '10:00 AM',
+        date: '2023-10-06'
+    },
+    {
+        name: 'Saturday',
+        heartBeat: 69,
+        cervicalDialation: 1,
+        contractions: 2,
+        time: '10:00 AM',
+        date: '2023-10-07'
+    },
+    {
+        name: 'Sunday',
+        heartBeat: 80,
+        cervicalDialation: 3,
+        contractions: 4,
+        time: '10:00 AM',
+        date: '2023-10-08'
+    }
+]
