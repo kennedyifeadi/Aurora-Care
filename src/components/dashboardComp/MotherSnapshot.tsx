d272f3566a7aafc453a1e6377918abe29f0b0048
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-08-26 12:38:48 +0100

    fix: adjust formatting in MotherSnapshot component for better readability

--- src/components/dashboardComp/MotherSnapshot.tsx
+++ src/components/dashboardComp/MotherSnapshot.tsx
@@ -76,7 +76,7 @@ export const MotherSnapshot = () => {
                     <div className="flex justify-between">
                         <span className="flex items-center gap-2 text-[18px] font-medium">
                             Contractions
-                        <IoAnalytics className="text-[#7e22ce]" />
+                            <IoAnalytics className="text-[#7e22ce]" />
                         </span>
                         <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M10 19.75C15.1086 19.75 19.25 15.6086 19.25 10.5C19.25 5.39137 15.1086 1.25 10 1.25C4.89137 1.25 0.75 5.39137 0.75 10.5C0.75 15.6086 4.89137 19.75 10 19.75Z" stroke="black" strokeOpacity="0.48" strokeWidth="1.5" />
@@ -101,7 +101,7 @@ export const MotherSnapshot = () => {
                     <div className="w-full h-max">
                         <MotherContractionChart />
                     </div>
-                     <div>
+                    <div>
                         <p >
                             {contractionMessage} | <span className="font-medium text-[#15AF63] w-max flex justify-center items-center"><FaLongArrowAltUp />{shortContractionMessage}</span>
                         </p>
@@ -121,7 +121,7 @@ export const MotherSnapshot = () => {
                             <path d="M10 8.18799C10.6904 8.18799 11.25 7.62834 11.25 6.93799C11.25 6.24763 10.6904 5.68799 10 5.68799C9.30964 5.68799 8.75 6.24763 8.75 6.93799C8.75 7.62834 9.30964 8.18799 10 8.18799Z" fill="black" fillOpacity="0.48" />
                         </svg>
                     </div>
-                                        <div className="flex flex-col">
+                    <div className="flex flex-col">
                         <span className="text-[32px] md:text-[48px] font-bold text-[#7e22ce]">
                             {
                                 currentCervicalDilation
