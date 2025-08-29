d765aae4cb0a1a5abbe00651be18e21da1b59b2f
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-08-29 08:24:52 +0100

    feat: update Mother component layout to improve spacing and include VitalsCard

--- src/pages/Mother.tsx
+++ src/pages/Mother.tsx
@@ -1,10 +1,12 @@
 import { ProfileCard } from "../components/motherComp/ProfileCard"
+import { VitalsCard } from "../components/motherComp/VitalsCard"
 
 export const Mother = () => {
   return (
     <div className="flex flex-col w-full gap-4 p-4">
-      <div className="flex gap-2 w-full h-max ">
+      <div className="flex gap-8 w-full h-max ">
         <ProfileCard />
+        <VitalsCard />
       </div>
       <div></div>
       <div></div>
