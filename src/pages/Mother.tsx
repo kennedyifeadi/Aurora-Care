874aaea4bd995d761fea8c43089235a780b680d5
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-08-31 15:04:01 +0100

    feat: restructure Mother component for improved layout of real-time vitals displays

--- src/pages/Mother.tsx
+++ src/pages/Mother.tsx
@@ -1,4 +1,10 @@
 import { ProfileCard } from "../components/motherComp/ProfileCard"
+import { BloodPressureDisplay } from "../components/motherComp/realTimeCards/BloodPressure"
+import { CervicalDilationDisplay } from "../components/motherComp/realTimeCards/CervicalDilation"
+import { ContractionsDisplay } from "../components/motherComp/realTimeCards/Contractions"
+import { HeartRateDisplay } from "../components/motherComp/realTimeCards/HeartRate"
+import { OxygenSaturationDisplay } from "../components/motherComp/realTimeCards/OxygenSaturation"
+import { TemperatureDisplay } from "../components/motherComp/realTimeCards/Temperature"
 import { VitalsCard } from "../components/motherComp/VitalsCard"
 
 export const Mother = () => {
@@ -10,8 +16,17 @@ export const Mother = () => {
       </div>
       <div className="flex flex-col flex-wrap gap-4">
         <h1 className="text-[20px] md:text-[24px] font-bold">Real-Time Vitals Monitoring</h1>
-        <div>
-
+        <div className="flex flex-col gap-8">
+          <div className="flex gap-8 flex-wrap md:justify-start justify-center">
+            <TemperatureDisplay />
+            <HeartRateDisplay />
+            <BloodPressureDisplay />
+          </div>
+          <div className="flex gap-8 flex-wrap md:justify-start justify-center">
+            <OxygenSaturationDisplay />
+            <CervicalDilationDisplay />
+            <ContractionsDisplay />
+          </div>
         </div>
       </div>
       <div></div>
